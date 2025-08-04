fa59f3e075c74c849475096454f907a351e69096
LateOne15 <48002046+LateOne15@users.noreply.github.com>
2025-08-04 13:40:11 +0800

    Menu expansion
    Added options, menu explanation, and loop/exit function

--- LibraryMenu/src/main/java/com/mycompany/librarymenu/LibraryMenu.java
+++ LibraryMenu/src/main/java/com/mycompany/librarymenu/LibraryMenu.java
@@ -15,21 +15,40 @@ public class LibraryMenu {
         Scanner sc = new Scanner(System.in);
         int keyIn = 0;
         
-        System.out.println("Overview of menu options");
-        keyIn = sc.nextInt();
-        switch(keyIn){
-            case 0 -> {
-                System.out.println("Menu A");
+        System.out.println("Welcome to the library system!");
+        do {
+            System.out.println("Library System Menu:");
+            System.out.println("1. Borrow a book");
+            System.out.println("2. Return a book");
+            System.out.println("3. View catalogue");
+            System.out.println("4. Pay late fees");
+            System.out.println("0. Exit program");
+            System.out.print("Please input your choice: ");
+            keyIn = sc.nextInt();
+            switch(keyIn){
+                case 0 -> {
+                    System.out.println("Thank you for stopping by!");
+                }
+                case 1 -> {
+                    System.out.println("Please enjoy your book!");
+                }
+                case 2 -> {
+                    System.out.println("Thank you for returning this book.");
+                }
+                case 3 -> {
+                    System.out.println("Here is our selection:");
+                    System.out.println("Indentation and you - by Me");
+                    System.out.println("Joke options and where not to use them - by You");
+                    System.out.println("That concludes our catalogue.");
+                }
+                case 4 -> {
+                    System.out.println("Thank you for paying the fees.");
+                    System.out.println("We encourage you to not let your books become overdue next time.");
+                }
+                default -> {
+                    System.out.println("This wasn't meant to happen - consider it an error.");
+                }
             }
-            case 1 -> {
-                System.out.println("Menu B");
-            }
-            case 2 -> {
-                System.out.println("Menu C");
-            }
-            case 3 -> {
-                System.out.println("Menu D");
-            }
-        }
+        } while (keyIn != 0);
     }
 }
